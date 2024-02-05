fd2a6fbd8280e4762c2a7d921fb7e1931e813c64
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-05 18:15:11 +0100

    snack1 - inserimento ciclo while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,28 @@
 
 // Continua a chiedere i numeri all’utente e a inserirli nell’array
 // fino a quando la somma degli elementi è minore di 50
+
+
+// array vuoto 
+let numbersArray = [];
+
+// inizializzo la somma 
+let sum = 0;
+
+
+
+// Finché la somma è < 50
+while (sum < 50) {
+    // chiedo di inserire un numero 
+    let number = Number(prompt("inserire un numero:"));
+
+    // inserisco il numero nell'array 
+    numbersArray.push(number);
+    // lo sommo per vedere se è < 50
+    sum += number;
+
+
+    // log
+    console.log(sum, number, numbersArray);
+}
+
