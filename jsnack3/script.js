58bb03b6fe52cdea21da388cabfcde3e75b9f534
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-05 20:45:31 +0100

    snack3 - risultatao in pagina

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,8 +3,10 @@
 // (Se iniziate con un array da 3 elementi e l'altro da 5, eseguite istruzioni sinchè non ne hanno entrambi 5)
 
 
-// creo 2 array 
+// creo 2 array
+// nb:possono contenere tutti gli elementi che si desidera
 const firstArray = [1, 2, 3];
+
 const secondArray = [1, 2, 3, 4, 5];
 
 // finché nel primo array ci sono meno elementi 
@@ -15,6 +17,17 @@ while (firstArray.length < secondArray.length){
 }
 
 
+// finché nel secondo array ci sono meno elementi 
+while (firstArray.length > secondArray.length){
+    // inserisco un numero 
+    let userWord = Number(prompt("Inserire un numero nel primo gruppo."));
+    secondArray.push(userWord);
+}
+
+document.getElementById("result").innerHTML = "i numeri del primo gruppo sono: " + firstArray + "<br>I numeri del secondo gruppo sono: " + secondArray;
 
 
-console.log(firstArray, secondArray);
+// log 
+console.log(
+    firstArray, 
+    secondArray);
